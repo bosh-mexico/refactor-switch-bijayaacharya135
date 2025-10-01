ae5cab04f50d1c0f6f0b274108dcb1fdb94a9f1a
bijayaacharya135 <internbijaya@gmail.com>
2025-10-01 14:41:46 +0530

    main.cpp

--- main.cpp
+++ main.cpp
@@ -6,16 +6,6 @@
 #include <iostream>
 #include <string>
 
-// Registers all payment processors with the registry
-void registerProcessors() {
-    PaymentRegistry::instance().registerProcessor(
-        PaymentMode::PayPal, std::make_unique<PayPalProcessor>());
-    PaymentRegistry::instance().registerProcessor(
-        PaymentMode::GooglePay, std::make_unique<GooglePayProcessor>());
-    PaymentRegistry::instance().registerProcessor(
-        PaymentMode::CreditCard, std::make_unique<CreditCardProcessor>());
-}
-
 int main() {
     registerProcessors();
 
